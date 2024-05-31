4b1dd55fac96b49474500c08cdbe40cb62413c14
LuizCarlxs1 <166883697+LuizCarlxs1@users.noreply.github.com>
2024-05-30 22:08:18 -0300

    Add files via upload

---
+++ Pergunstas_Esportes/perguntas.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    question: "Em uma partida de futebol, quantos jogadores de uma equipe entram em campo?",
+    answers: [
+      { option: "11", correct: true },
+      { option: "8", correct: false },
+      { option: "25", correct: false },
+      { option: "50", correct: false },
+    ],
+  },
+  {
+    question: "Quantas vezes o Brasil ganhou a copa do mundo?",
+    answers: [
+      { option: "100", correct: false },
+      { option: "3", correct: false },
+      { option: "1", correct: false },
+      { option: "5", correct: true },
+    ],
+  },
+  {
+    question: "Como se chama o esporte que é realizado em cima de um cavalo??",
+    answers: [
+      { option: "Moto cross", correct: false },
+      { option: "Ciclismo", correct: false },
+      { option: "Voleibol", correct: false },
+      { option: "Hipismo", correct: true },
+    ],
+  },
+  {
+    question: "Quantos jogadores tem em um jogo de basquete?",
+    answers: [
+      { option: "4", correct: false },
+      { option: "7", correct: false },
+      { option: "5", correct: true },
+      { option: "6", correct: false },
+    ],
+  },
+];
