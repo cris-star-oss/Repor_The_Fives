2a18b3b570ebb946dd62844a0c620dbccf723265
Lucas Nevada <134811741+DonoDaLua@users.noreply.github.com>
2024-06-02 13:07:46 -0300

    perguntas.js

---
+++ Perguntas_Artes/perguntas.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    question: "A pintura noite estrelada pertence a qual pintor?",
+    answers: [
+      { option: "Da Vinci", correct: false },
+      { option: "Van Gogh", correct: true },
+      { option: "Renoir", correct: false },
+      { option: "Picasso", correct: false },
+    ],
+  },
+  {
+    question: "Quantas cores tem no arco-iris?",
+    answers: [
+      { option: "4", correct: false },
+      { option: "8", correct: false },
+      { option: "7", correct: true },
+      { option: "5", correct: false },
+    ],
+  },
+  {
+    question: "Qual desses artista NÃO participou do renascentismo?",
+    answers: [
+      { option: "Brunelleschi", correct: false },
+      { option: "Romero Brito", correct: true },
+      { option: "Botticelli", correct: false },
+      { option: "Da Vinci", correct: false },
+    ],
+  },
+  {
+    question: "Em que ano surgiu o iluminismo?",
+    answers: [
+      { option: "1974", correct: false },
+      { option: "1755", correct: false },
+      { option: "2000", correct: false },
+      { option: "1715", correct: true },
+    ],
+  },
+];
