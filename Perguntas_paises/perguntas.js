857259a37092f013ba89e1cf5dbb07f4e0351c0c
Lucas Nevada <134811741+DonoDaLua@users.noreply.github.com>
2024-06-02 13:16:28 -0300

    perguntas.js

---
+++ Perguntas_paises/perguntas.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    question: "Qual a capital do Brasil?",
+    answers: [
+      { option: "Rio de Janeiro", correct: false },
+      { option: "Brasília", correct: true },
+      { option: "São paulo", correct: false },
+      { option: "Destrito federal", correct: false },
+    ],
+  },
+  {
+    question: "Em qual país fica a torre pisa?",
+    answers: [
+      { option: "Itália", correct: true },
+      { option: "Espanha", correct: false },
+      { option: "França", correct: false },
+      { option: "Brasil", correct: false },
+    ],
+  },
+  {
+    question: "Quantos continentes existem?",
+    answers: [
+      { option: "6", correct: true },
+      { option: "5", correct: false },
+      { option: "7", correct: false },
+      { option: "8", correct: false },
+    ],
+  },
+  {
+    question: "Quantos Países existem na Africa?",
+    answers: [
+      { option: "65", correct: false },
+      { option: "50", correct: false },
+      { option: "54", correct: true },
+      { option: "87", correct: false },
+    ],
+  },
+];
